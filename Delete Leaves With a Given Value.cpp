db0def6d09289cb1709ed3687e3d023827d28f58
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2023-07-12 23:23:17 +0530

    Create Delete Leaves With a Given Value.cpp

---
+++ Delete Leaves With a Given Value.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int t) 
+    {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        root->left=removeLeafNodes(root->left,t);
+        root->right=removeLeafNodes(root->right,t);
+        if(root->left==NULL && root->right==NULL && root->val==t)
+        {
+            return NULL;
+        }
+        return root;
+    }
+};
