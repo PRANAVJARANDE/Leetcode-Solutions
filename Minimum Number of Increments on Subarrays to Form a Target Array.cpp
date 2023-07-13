b4171ab9a1985106925d5876b43042b23e705bab
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2023-07-13 17:52:26 +0530

    Create Minimum Number of Increments on Subarrays to Form a Target Array.cpp

---
+++ Minimum Number of Increments on Subarrays to Form a Target Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& arr) 
+    {
+        int ans=arr[0];
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]>arr[i-1])
+            {
+                ans+=arr[i]-arr[i-1];
+            }
+        }
+        return ans;
+    }
+};
