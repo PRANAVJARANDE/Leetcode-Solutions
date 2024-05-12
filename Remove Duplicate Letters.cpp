78708c9e1f21d121b3d46018649eb36daaba94a6
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2024-05-12 17:13:22 +0530

    Create Remove Duplicate Letters.cpp

---
+++ Remove Duplicate Letters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) 
+    {
+        int n=s.size();
+        vector<int> vis(26,0);
+        vector<int>li(26,0);
+        for(int i=0;i<n;i++)
+        {
+            li[s[i]-'a']=i;
+        }
+
+        stack<char> s1;
+        for(int i=0;i<n;i++)
+        {
+            if(vis[s[i]-'a']==1)continue;
+            vis[s[i]-'a']=1;
+
+            while(!s1.empty() && s1.top()>s[i] && i<li[s1.top()-'a'])
+            {
+                vis[s1.top()-'a']=0;
+                s1.pop();
+            }
+            s1.push(s[i]);
+        }
+        int sz=s1.size();
+        string ans(sz,'0');
+        int in=s1.size()-1;
+        while(in>=0)
+        {
+            ans[in]=s1.top();
+            s1.pop();
+            in--;
+        }
+        return ans;
+        
+    }
+};
