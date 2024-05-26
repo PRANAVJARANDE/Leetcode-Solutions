f643ccec5c1dc60d120d0da545b96057781fee6f
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2024-05-26 09:49:14 +0530

    Create Find the Number of Good Pairs I.cpp

---
+++ Find the Number of Good Pairs I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int ans=0;
+        for(int x: nums1)
+        {
+            for(int y:nums2)
+            {
+                int h=k*y;
+                if(x%h==0)ans++;
+            }
+        }
+        return ans;
+    }
+};
