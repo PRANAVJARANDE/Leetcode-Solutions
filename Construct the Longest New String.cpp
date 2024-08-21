663f3f6e662ed2ce83e5fbd77347c548348ab4d4
PJARANDE <pranavjarande@gmail.com>
2024-08-22 00:04:54 +0530

    Update Construct the Longest New String.cpp

--- Construct the Longest New String.cpp
+++ Construct the Longest New String.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    vector<vector<vector<vector<int>>>> dp;
-    int helper(int x, int y, int z, int prev) 
-    {
-        if (dp[x][y][z][prev] != -1) {
-            return dp[x][y][z][prev];
-        }
-
-        int op = 0;
-        if (prev == 1) {
-            if (y > 0) {
-                op = 2 + helper(x, y - 1, z, 2);
-            }
-        } else {
-            if (x > 0) {
-                op = max(op, 2 + helper(x - 1, y, z, 1));
-            }
-            if (z > 0) {
-                op = max(op, 2 + helper(x, y, z - 1, 3));
-            }
-        }
-
-        return dp[x][y][z][prev] = op;
-    }
 
     int longestString(int x, int y, int z) {
-        dp.resize(x + 1, vector<vector<vector<int>>>(y + 1, vector<vector<int>>(z + 1, vector<int>(4, -1))));
-
-        return max({2 + helper(x - 1, y, z, 1),
-                    2 + helper(x, y - 1, z, 2),
-                    2 + helper(x, y, z - 1, 3)});
+        int ans=0;
+        if(z==0)
+        {
+            if(x==y)return 4*min(x,y);
+            else return 4*min(x,y)+2;
+        }
+        else
+        {
+            int ans=2*z+4*min(x,y);
+            if(x!=y)ans+=2;
+            return ans;
+        }
     }
 };
