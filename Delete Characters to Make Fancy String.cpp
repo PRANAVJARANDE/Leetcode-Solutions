a9b408f6cd3d1f66fc3c418693f698f1fb99efa9
PJARANDE <pranavjarande@gmail.com>
2024-11-01 13:12:36 +0530

    Create Delete Characters to Make Fancy String.cpp

---
+++ Delete Characters to Make Fancy String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int n=s.size();
+        if(n<=2)return s;
+        string ans="";
+        ans+=s[0];
+        ans+=s[1];
+        for(int i=2;i<s.size();i++)
+        {
+            if(s[i]==s[i-1] && s[i]==s[i-2])continue;
+            else ans+=s[i];
+        }
+        return ans;
+    }
+};
