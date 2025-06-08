b2c42125ef1945f5f32e8a61fc1a4a4ce0344b71
pj17 <pranavjarande@gmail.com>
2025-06-08 14:13:49 +0530

    Update Lexicographical Numbers.cpp

--- medium/Lexicographical Numbers.cpp
+++ medium/Lexicographical Numbers.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Brute Force
+class Solution1 {
 public:
     vector<int> lexicalOrder(int n) 
     {
@@ -16,3 +17,45 @@ public:
         return ans;
     }
 };
+
+//Sort Function 
+class Solution2 {
+public:
+
+    static bool cmp(int a,int b)return to_string(a)<to_string(b);
+
+    vector<int> lexicalOrder(int n) 
+    {
+        vector<int> ans;
+        for(int i=1;i<=n;i++)ans.push_back(i);
+        sort(ans.begin(),ans.end(),cmp);
+        return ans;
+    }
+};
+
+// o(n) time complexity and o(1)space complecxtiy
+class Solution {
+public:
+    void dfs(int curr,int n,vector<int> &ans) 
+    {
+        if(curr>n)return;
+        ans.push_back(curr);
+        for (int i=0;i<=9;i++) 
+        {
+            if(curr*10+i>n)break;
+            dfs(curr*10+i,n,ans);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) 
+    {
+        vector<int> ans;
+        for(int i=1;i<=9;i++) 
+        {
+            if(i>n)break;
+            dfs(i,n,ans);
+        }
+        return ans;
+    }
+};
+
