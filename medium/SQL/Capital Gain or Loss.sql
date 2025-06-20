b2b7ed723fe011b9f249477c38ae83d6fc977832
pj17 <pranavjarande@gmail.com>
2025-06-20 18:16:10 +0530

    Update Capital Gain or Loss.sql

--- medium/SQL/Capital Gain or Loss.sql
+++ medium/SQL/Capital Gain or Loss.sql
@@ -1,3 +1,4 @@
+--OPTION 1
 SELECT s.stock_name as stock_name,( 
     SELECT SUM(price)
     FROM Stocks
@@ -9,3 +10,22 @@ SELECT s.stock_name as stock_name,(
 ) as capital_gain_loss
 FROM Stocks as s 
 GROUP BY s.stock_name
+
+
+--OPTION 2
+WITH 
+selling as (
+    SELECT stock_name , SUM(price) as sell_price
+    FROM Stocks 
+    WHERE operation="Sell"
+    GROUP BY stock_name    
+),
+buying as (
+    SELECT stock_name  , SUM(price) as buy_price
+    FROM Stocks 
+    WHERE operation="Buy"
+    GROUP BY stock_name 
+)
+SELECT s.stock_name , (s.sell_price -  b.buy_price) as capital_gain_loss 
+FROM selling as  s
+JOIN buying as b on s.stock_name=b.stock_name 
