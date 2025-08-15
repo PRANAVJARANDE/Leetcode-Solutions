f43e74bcb06be2fc263f07ed2d565974c53746f2
pj17 <pranavjarande@gmail.com>
2025-08-15 15:49:43 +0530

    Update Insert Interval.cpp

--- medium/Insert Interval.cpp
+++ medium/Insert Interval.cpp
@@ -1,3 +1,41 @@
+// GFG Solution 
+class Solution {
+  public:
+    vector<vector<int>> insertInterval(vector<vector<int>> &a, vector<int> &ni) {
+        vector<vector<int>>ans;
+        int nx=ni[0];
+        int ny=ni[1];
+        int n=a.size();
+        
+        if(ny<a[0][0])ans.push_back({nx,ny});
+        for(int i=0;i<n;i++)
+        {
+            if(!ans.empty() && ans.back()[1]<nx && ny<a[i][0])
+            {
+                ans.push_back({nx,ny});
+            }
+            if(a[i][1]<nx)ans.push_back(a[i]);
+            else if(ny<a[i][0])ans.push_back(a[i]);
+            else
+            {
+                int sx=min(nx,a[i][0]);
+                int sy=ny;
+                while(i<n && a[i][0]<=ny)
+                {
+                    sy=max(sy,a[i][1]);
+                    i++;
+                }
+                ans.push_back({sx,sy});
+                i--;
+            }
+        }
+        if(a[n-1][1]<nx)ans.push_back({nx,ny});
+        return ans;
+    }
+};
+
+
+//Leetcode Solution
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& n) 
