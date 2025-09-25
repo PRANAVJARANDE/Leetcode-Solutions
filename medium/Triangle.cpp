6c80086afa40f245a4e9b6f7235c8622c413f1d5
pj17 <pranavjarande@gmail.com>
2025-09-25 14:57:18 +0530

    Update Triangle.cpp

--- medium/Triangle.cpp
+++ medium/Triangle.cpp
@@ -1,3 +1,28 @@
+//Optimized space complexity
+class Solution {
+public:
+
+    int minimumTotal(vector<vector<int>>& a) 
+    {
+        int n=a.size();
+        int ans=a[0][0];
+        for(int i=1;i<n;i++)
+        {
+            ans=INT_MAX;
+            for(int j=0;j<a[i].size();j++)
+            {
+                int op1=INT_MAX,op2=INT_MAX;
+                if(j-1>=0)op1=a[i][j]+a[i-1][j-1];
+                if(j!=a[i].size()-1)op2=a[i][j]+a[i-1][j];
+                a[i][j]=min(op1,op2);
+                ans=min(ans,a[i][j]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Not optimized space complexity
 class Solution {
 public:
 
